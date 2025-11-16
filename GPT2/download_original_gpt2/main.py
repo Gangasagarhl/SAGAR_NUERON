408b9776de75f29de401cf3d52c36e4b0b3e7747
Gangasagarhl <gangasagarhl1112@gmail.com>
2025-11-16 13:58:35 +0530

    openai weights loading 1

--- GPT2/download_original_gpt2/main.py
+++ GPT2/download_original_gpt2/main.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import tqdm
 from download_original_gpt2.download_weights import download_and_load_gpt2
 from sagar_neuron_gpt2.model_configuration.gpt_model import GPTModel
-from sagar_neuron_gpt2.TrainAndSaveGptWeights import EvaluateTrainModel
 import torch
 import numpy as np
 import tiktoken
@@ -141,6 +140,46 @@ class GPT2Loader:
         self.override_weights()
         self.gpt.to(self.device)
 
+
+    def generate(self, model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None):
+
+        # For-loop is the same as before: Get logits, and only focus on last time step
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -context_size:]
+            with torch.no_grad():
+                logits = model(idx_cond)
+            logits = logits[:, -1, :]
+
+            # New: Filter logits with top_k sampling
+            if top_k is not None:
+                # Keep only top_k values
+                top_logits, _ = torch.topk(logits, top_k)
+                min_val = top_logits[:, -1]
+                logits = torch.where(logits < min_val, torch.tensor(float("-inf")).to(logits.device), logits)
+
+            # New: Apply temperature scaling
+            if temperature > 0.0:
+                logits = logits / temperature
+
+                # Apply softmax to get probabilities
+                probs = torch.softmax(logits, dim=-1)  # (batch_size, context_len)
+
+                # Sample from the distribution
+                idx_next = torch.multinomial(probs, num_samples=1)  # (batch_size, 1)
+
+            # Otherwise same as before: get idx of the vocab entry with the highest logits value
+            else:
+                idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch_size, 1)
+
+            if idx_next == eos_id:  # Stop generating early if end-of-sequence token is encountered and eos_id is specified
+                break
+
+            # Same as before: append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)  # (batch_size, num_tokens+1)
+
+        return idx
+    
+
     def initialise(self):
         self.load_model()
         self.check(self.settings, self.params)  # Uncomment to check settings and params
@@ -150,17 +189,41 @@ class GPT2Loader:
 
         return self.gpt
     
-    def let_the_model_speak(self):
-        eva = EvaluateTrainModel()
-        eva.generate_and_print_sample(self.gpt, self.tokenizer, device=self.device, start_context="Who is Yash in Karnataka?" )
-        
+    def text_to_token_ids(self,text, tokenizer):
+        encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+        encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimension
+        return encoded_tensor
+
+    def token_ids_to_text(self,token_ids, tokenizer):
+        flat = token_ids.squeeze(0) # remove batch dimension
+        return tokenizer.decode(flat.tolist())
+    
+
+    def let_the_model_speak(self, ask):
+        torch.manual_seed(123)
+
+        token_ids = self.generate(
+            model=model,
+            idx=self.text_to_token_ids(ask, self.tokenizer),
+            max_new_tokens=15,
+            context_size=self.GPT_CONFIG_124M["context_length"],
+            top_k=25,
+            temperature=1.4
+        )
+
+        print("Output text:\n", self.token_ids_to_text(token_ids, self.tokenizer))
+
+    
+
 
 if __name__ == "__main__":
     gpt2_loader = GPT2Loader(model_size="124M", models_dir="gpt2")
     model = gpt2_loader.initialise()
     print("Model loaded and weights overridden successfully.")
-    gpt2_loader.let_the_model_speak()
-    
+
+    ask = input("Chat with model:\nYou: ")
+    gpt2_loader.let_the_model_speak(ask)
+
 
 
 
