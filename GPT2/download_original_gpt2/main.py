883f2259cfe47877feb94252a45a5baf962af67c
Gangasagarhl <gangasagarhl1112@gmail.com>
2025-11-16 12:47:32 +0530

    openai weights loading done

---
+++ GPT2/download_original_gpt2/main.py
@@ -0,0 +1,182 @@
+import tensorflow as tf
+import tqdm
+from download_original_gpt2.download_weights import download_and_load_gpt2
+from sagar_neuron_gpt2.model_configuration.gpt_model import GPTModel
+from sagar_neuron_gpt2.TrainAndSaveGptWeights import EvaluateTrainModel
+import torch
+import numpy as np
+import tiktoken
+
+
+class GPT2Loader:   
+    
+    def __init__(self, model_size="124M", models_dir="gpt2"):
+        self.model_size = model_size
+        self.models_dir = models_dir
+        self.GPT_CONFIG_124M = {
+            "vocab_size": 50257,    # Vocabulary size
+            "context_length": 1024, # Context length
+            "emb_dim": 768,         # Embedding dimension
+            "n_heads": 12,          # Number of attention heads
+            "n_layers": 12,         # Number of layers
+            "drop_rate": 0.1,       # Dropout rate
+            "qkv_bias": False       # Query-Key-Value bias
+            }
+        self.gpt =None
+        self.settings =None 
+        self.params =None
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+    
+    def load_model(self):
+        self.settings, self.params = download_and_load_gpt2(model_size=self.model_size, models_dir=self.models_dir)
+
+
+   
+
+    
+    def check(self, settings, params):
+        print("Settings: ", settings)
+        print("Parameter dictioanry  keys: ", params.keys())
+
+        print("\n\n")
+
+        print(params['wte'])
+        print("Token embedding weight dimensions: ", params['wte'].shape)
+
+    
+    #This is used for implemnting the model with the radom weights
+    def implement_model(self):
+        model_configs =         {
+        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+        }
+
+        # Copy the base configuration and update with specific model settings
+        model_name = "gpt2-small (124M)"  # Example model name
+        NEW_CONFIG = self.GPT_CONFIG_124M.copy()
+        NEW_CONFIG.update(model_configs[model_name])
+        NEW_CONFIG.update({"context_length": 1024, "qkv_bias": True})
+        self.gpt = GPTModel(NEW_CONFIG)
+        self.tokenizer = tiktoken.get_encoding("gpt2")
+        
+    def eval_gpt_model(self):
+        self.gpt.eval()
+    
+
+    def helper_to_override(self,left, right):
+        if left.shape != right.shape:
+            raise ValueError(f"Shape mismatch. Left: {left.shape}, Right: {right.shape}")
+        return torch.nn.Parameter(torch.tensor(right))
+    
+    def override_weights(self):
+
+        gpt = self.gpt
+        assign = self.helper_to_override
+        params = self.params
+
+        gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params['wpe'])
+        gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params['wte'])
+        
+        for b in range(len(params["blocks"])):
+            q_w, k_w, v_w = np.split(
+                (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+            gpt.trf_blocks[b].att.W_query.weight = assign(
+                gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+            gpt.trf_blocks[b].att.W_key.weight = assign(
+                gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+            gpt.trf_blocks[b].att.W_value.weight = assign(
+                gpt.trf_blocks[b].att.W_value.weight, v_w.T)
+
+            q_b, k_b, v_b = np.split(
+                (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+            gpt.trf_blocks[b].att.W_query.bias = assign(
+                gpt.trf_blocks[b].att.W_query.bias, q_b)
+            gpt.trf_blocks[b].att.W_key.bias = assign(
+                gpt.trf_blocks[b].att.W_key.bias, k_b)
+            gpt.trf_blocks[b].att.W_value.bias = assign(
+                gpt.trf_blocks[b].att.W_value.bias, v_b)
+
+            gpt.trf_blocks[b].att.out_proj.weight = assign(
+                gpt.trf_blocks[b].att.out_proj.weight, 
+                params["blocks"][b]["attn"]["c_proj"]["w"].T)
+            gpt.trf_blocks[b].att.out_proj.bias = assign(
+                gpt.trf_blocks[b].att.out_proj.bias, 
+                params["blocks"][b]["attn"]["c_proj"]["b"])
+
+            gpt.trf_blocks[b].ff.layers[0].weight = assign(
+                gpt.trf_blocks[b].ff.layers[0].weight, 
+                params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+            gpt.trf_blocks[b].ff.layers[0].bias = assign(
+                gpt.trf_blocks[b].ff.layers[0].bias, 
+                params["blocks"][b]["mlp"]["c_fc"]["b"])
+            gpt.trf_blocks[b].ff.layers[2].weight = assign(
+                gpt.trf_blocks[b].ff.layers[2].weight, 
+                params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+            gpt.trf_blocks[b].ff.layers[2].bias = assign(
+                gpt.trf_blocks[b].ff.layers[2].bias, 
+                params["blocks"][b]["mlp"]["c_proj"]["b"])
+
+            gpt.trf_blocks[b].norm1.scale = assign(
+                gpt.trf_blocks[b].norm1.scale, 
+                params["blocks"][b]["ln_1"]["g"])
+            gpt.trf_blocks[b].norm1.shift = assign(
+                gpt.trf_blocks[b].norm1.shift, 
+                params["blocks"][b]["ln_1"]["b"])
+            gpt.trf_blocks[b].norm2.scale = assign(
+                gpt.trf_blocks[b].norm2.scale, 
+                params["blocks"][b]["ln_2"]["g"])
+            gpt.trf_blocks[b].norm2.shift = assign(
+                gpt.trf_blocks[b].norm2.shift, 
+                params["blocks"][b]["ln_2"]["b"])
+
+        gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+        gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+        gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+
+    def get_model(self):
+        self.override_weights()
+        self.gpt.to(self.device)
+
+    def initialise(self):
+        self.load_model()
+        self.check(self.settings, self.params)  # Uncomment to check settings and params
+        self.implement_model()
+        self.eval_gpt_model()
+        self.get_model()
+
+        return self.gpt
+    
+    def let_the_model_speak(self):
+        eva = EvaluateTrainModel()
+        eva.generate_and_print_sample(self.gpt, self.tokenizer, device=self.device, start_context="Who is Yash in Karnataka?" )
+        
+
+if __name__ == "__main__":
+    gpt2_loader = GPT2Loader(model_size="124M", models_dir="gpt2")
+    model = gpt2_loader.initialise()
+    print("Model loaded and weights overridden successfully.")
+    gpt2_loader.let_the_model_speak()
+    
+
+
+
+    
+
+        
+
+
+
+    
+
+    
+    
+    
+    
+
+
+        
+        
